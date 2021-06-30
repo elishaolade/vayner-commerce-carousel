271a5001698813378c95459742250361b9c3cbd6
Elisha Olade <elishaolade@gmail.com>
2021-06-30 16:38:50 -0700

    created Slide component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import prevIcon from '.././assets/arrow-left.svg';
 import nextIcon from '.././assets/arrow-right.svg';
 import './Carousel.scss';
+import Slide from './Slide';
 
 const Carousel = (props) => {
     const [isMobile, setMobile] = React.useState(true);
@@ -9,6 +10,7 @@ const Carousel = (props) => {
     const limit = props.data.length;
     useEffect(()=>{
         const media = window.matchMedia('(max-width: 600px)');
+
         if(media.matches){
             setMobile(isMobile);
         }
@@ -24,10 +26,6 @@ const Carousel = (props) => {
             }
         }
         media.addEventListener('change', checkScreen);
-        const intervalId = setInterval(() => {
-            next()
-          }, 4000)
-          return () => clearInterval(intervalId)
     }, [])
     const next = () => {
         setIndex(index < limit - 1 ? index + 1 : 0);
@@ -35,9 +33,7 @@ const Carousel = (props) => {
     const prev = () => {
         setIndex(index > 0 ? index - 1 : limit - 1)
     };
-    const rotate = () => {
-        next();
-    }
+
 
     return(
         <div className="Carousel">
@@ -45,29 +41,7 @@ const Carousel = (props) => {
                 <div className="Carousel__slides" style={{ transform: `translateX(${index * -100}%)`}}>
                     {props.data.map((slide,index) => {
                         const background = { backgroundImage: isMobile ? `url(${slide.media.mobile})` : `url(${slide.media.desktop})` }
-                        return (
-                            <div className="Slide" key={index} style={background}>
-                            <div className={`${!isMobile? 'Slide__inner--' + slide.ctaPosition : 'Slide__inner'}`}>
-                                <div className="Slide__content-wrapper">
-                                    <h1 className="Slide__title">{slide.title}</h1>
-                                    { slide.heading && <h6 className="Slide__heading">{slide.heading}</h6> }
-                                    { slide.subhead && <h6 className="Slide__subhead">{slide.subhead}</h6> }
-                                    <div className={`Slide__cta-wrapper--${slide.cta.length > 1 ? 'double':'single'}`}>
-                                    { slide.cta.map(cta => {
-                                        return (
-                                            <div className="Slide__cta">
-                                                <h5 className="Slide__label"></h5>
-                                                <div className="Slide__btn-wrapper">
-                                                    <a href={cta.url} className="Slide__btn">{cta.label}</a>
-                                                </div>
-                                            </div>
-                                        )
-                                    })}
-                                    </div>
-                                </div>
-                            </div>
-                            </div>
-                        )
+                        return <Slide key={props.index} index={index} slide={slide} background={background} isMobile={isMobile}/>
                     })}
                 </div>
                 <div className="Carousel__controls">
--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from 'react';
+import './Slide.scss';
 
 export const Slide = (props) => {
-    useEffect(() => {
-        effect
-        return () => {
-            cleanup
-        }
-    }, [input])
     return (
-        <div className="Slide">
-            
+        <div className="Slide" style={props.background}>
+            <div className={`${!props.isMobile? 'Slide__inner--' + props.slide.ctaPosition : 'Slide__inner'}`}>
+            <div className="Slide__content-wrapper">
+                <h1 className="Slide__title">{props.slide.title}</h1>
+                { props.slide.heading && <h6 className="Slide__heading">{props.slide.heading}</h6> }
+                { props.slide.subhead && <h6 className="Slide__subhead">{props.slide.subhead}</h6> }
+                <div className={`Slide__cta-wrapper--${props.slide.cta.length > 1 ? 'double':'single'}`}>
+                { props.slide.cta.map(cta => {
+                    return (
+                        <div className="Slide__cta">
+                            <h5 className="Slide__label"></h5>
+                            <div className="Slide__btn-wrapper">
+                                <a href={cta.url} className="Slide__btn">{cta.label}</a>
+                            </div>
+                        </div>
+                    )
+                })}
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
+
+export default Slide;
